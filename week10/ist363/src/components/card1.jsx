8abb9d36217985e4a25c7a550980e0ab233891cb
Skye MacLeod <asmacleo@syr.edu>
2026-03-24 08:21:37 -0400

    wr

---
+++ week10/ist363/src/components/card1.jsx
@@ -0,0 +1,9 @@
+function Card1() {
+    return (
+        <div className="card">
+            <h2>Card Name</h2>
+            <p>Card Description...</p>
+            <button className="btn"></button>
+        </div>
+    )
+}
